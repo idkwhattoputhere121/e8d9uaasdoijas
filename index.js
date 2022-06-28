6c38089fe22c105a9e479dc8dafc1a2a5235e247
idkwhattoputhere121 <75852933+idkwhattoputhere121@users.noreply.github.com>
2022-06-27 20:46:27 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const axios = require("axios");
+const fs = require("fs");
+
+setInterval(function () {
+  axios
+    .get("https://MuffledLightgreenModulus.0be.repl.co/mcgroups.html")
+    .then(function (response) {
+      fs.writeFile("index.html", response.data, function (err) {
+        if (err) {
+          return console.log(err);
+        }
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}, 10000);
