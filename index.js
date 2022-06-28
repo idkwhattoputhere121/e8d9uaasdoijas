05f02b5be1557f5a47ff512ddb441238fd226109
IAmNotHacking <71348579+IAmNotHacking@users.noreply.github.com>
2022-06-28 18:06:32 -0500

    Fix stuff 2

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
+/*
+ * @Authors: Bongo.lua, IAmNotHacking
+ * @Description: Monitor & Uptime keeper bot
+ * @Copyright: 2022 Bongo.lua
+*/
+
 const axios = require("axios");
 const fs = require("fs");
 const express = require('express')
-
-fetchPage();
-setInterval(fetchPage, 10000);
-
-function fetchPage() {
-    const URL = "https://MuffledLightgreenModulus.0be.repl.co/";
-    axios.get(URL).then(res => {
-        fs.writeFile("./public/index.html", res.data, err => {
-            if(err) return console.error(err);
-        });
-    }).catch(err => {
-        console.error(err);
-    });
-}
-
 
 const app = express();
 app.use(express.static('public'));
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
-};
+});
+
+fetch();
+setInterval(fetch, 10000);
+
+function fetch() {
+    axios.get(process.env.thej).then(res => {
+      fs.writeFile("./public/index.html", res.data, (err) => {
+        if (err) return console.error(err);
+      });
+    }).catch(err => {
+      console.error(err);
+    });
+}
+
+/*
+ * @Authors Bongo.lua, IAmNotHacking
+ * @Description: Monitor & Uptime keeper bot
+ * @Copyright 2022 Bongo.lua
+*/
