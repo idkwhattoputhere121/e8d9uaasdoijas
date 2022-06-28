988c69e0c1daf20d34ff99dc0aae24fe995a3b1f
IAmNotHacking <71348579+IAmNotHacking@users.noreply.github.com>
2022-06-27 22:38:23 -0500

    Fix stuff

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
 const axios = require("axios");
 const fs = require("fs");
 const express = require('express')
-setInterval(function () {
-  axios
-    .get("https://MuffledLightgreenModulus.0be.repl.co/")
-    .then(function (response) {
-      fs.writeFile("./index.html", response.data, function (err) {
-        if (err) {
-          return console.log(err);
-        }
-      });
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}, 10000);
 
+fetchPage();
+setInterval(fetchPage, 10000);
+
+function fetchPage() {
+    const URL = "https://MuffledLightgreenModulus.0be.repl.co/";
+    axios.get(URL).then(res => {
+        fs.writeFile("./public/index.html", res.data, err => {
+            if(err) return console.error(err);
+        });
+    }).catch(err => {
+        console.error(err);
+    });
+}
 
 
 const app = express();
 app.use(express.static('public'));
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-}
-    , function (err) {
-        console.log(err);
-    }
-);  
-app.listen(3000, function () {
+app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
-}
-);
+};
