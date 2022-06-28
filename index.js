758f325d66f1b52acd10b76ce0b8f9ec929d0703
idkwhattoputhere121 <75852933+idkwhattoputhere121@users.noreply.github.com>
2022-06-27 22:14:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 setInterval(function () {
   axios
-    .get("https://MuffledLightgreenModulus.0be.repl.co/mcgroups.html")
+    .get("https://MuffledLightgreenModulus.0be.repl.co/index.html")
     .then(function (response) {
       fs.writeFile("/index.html", response.data, function (err) {
         if (err) {
